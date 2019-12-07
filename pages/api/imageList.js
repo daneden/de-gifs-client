0b5923956be32fcc597032a6b608c3159410f7c0
Daniel Eden <dan.eden@me.com>
2019-12-07 21:02:44 +0000

    Initial commit

---
+++ pages/api/imageList.js
@@ -0,0 +1,28 @@
+import NetlifyAPI from 'netlify'
+
+require('dotenv').config()
+
+const netlify = new NetlifyAPI(process.env.NETLIFY_AUTH_TOKEN)
+
+export default async (req, res) => {
+  const files = await netlify
+    .listSiteFiles({
+      site_id: process.env.NETLIFY_SITE_ID,
+    })
+    .then(data => {
+      return data.map(({ id, size, ...rest }) => {
+        return {
+          id,
+          size,
+        }
+      })
+    })
+    .then(files => {
+      return files.sort((a, b) => {
+        return a.id.localeCompare(b.id)
+      })
+    })
+    .catch(err => console.error(err))
+
+  res.json(files)
+}
