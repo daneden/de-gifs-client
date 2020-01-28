821c2743b5013b0c073979b8463e7c4d4789e335
Daniel Eden <dan.eden@me.com>
2020-01-28 15:00:25 +0000

    Convert some modules to TypeScript, use intersection observer to lazyload images

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import Imgix from 'react-imgix'
+import useIntersectionObserver from '../hooks/useIntersectionObserver'
 import styles from './Image.css'
 
-const { useState } = React
+const { useEffect, useState } = React
 
 const Image = ({ src }) => {
   const [isHovered, setIsHovered] = useState(false)
+  const [onScreen, setOnScreen] = useState(false)
   const [hasLoaded, setHasLoaded] = useState(true)
+  const [ref, entry] = useIntersectionObserver({
+    rootMargin: '24px',
+    threshold: [0, 1],
+  })
   const baseUrl = isHovered ? 'de-gifs.netlify.com' : 'de-gif-netlify.imgix.net'
   const source = `https://${baseUrl}${src}`
 
   const isGif = src.match('gif$')
+
+  useEffect(() => {
+    if (entry?.intersectionRatio > 0) {
+      setOnScreen(true)
+    } else {
+      setOnScreen(false)
+    }
+  }, [entry])
 
   function onMouseOver(e) {
     setIsHovered(true)
@@ -36,17 +50,18 @@ const Image = ({ src }) => {
     >
       <Imgix
         className={[styles.image, !hasLoaded && styles.isLoading].join(' ')}
-        height={150}
+        height={onScreen ? 150 : 15}
         imgixParams={{
           auto: isHovered ? 'format' : 'compress',
           format: isHovered ? 'auto' : 'jpg',
         }}
         htmlAttributes={{
+          ref,
           loading: 'lazy',
           onLoad,
         }}
         src={source}
-        width={150}
+        width={onScreen ? 150 : 15}
         sizes={'25vw'}
       />
     </a>
